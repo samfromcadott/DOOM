39aa4655a5effdf75eb4aa921f1654fdbd5f3d70
Sam Jackson <smahstan@gmail.com>
2020-01-04 17:02:03 -0600

    Started converting C to Python

---
+++ linuxdoom-1.10/doomtype.py
@@ -0,0 +1,16 @@
+# DESCRIPTION:
+#	Simple basic typedefs, isolated here to make it easier
+#	 separating modules.
+
+MAXCHAR = ((char)0x7f)
+MAXSHORT = int(0x7fff)
+
+# Max pos 32-bit int.
+MAXINT = int(0x7fffffff)
+MAXLONG = int(0x7fffffff)
+MINCHAR = ((char)0x80)
+MINSHORT = int(0x8000)
+
+# Max negative 32-bit integer.
+MININT = int(0x80000000)
+MINLONG = int(0x80000000)
